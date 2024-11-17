f060b12c18bbb6b83fbee172e9af2163688e38bc
kalid-2024 <kalidmohammed236@gmail.com>
2024-11-17 22:22:59 +0300

    Correction Navbar and Navbar.jsx file

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -1,15 +1,18 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
-      <Link to="/" style={{ margin: '0 15px' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 15px' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 15px' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 15px' }}>Contact</Link>
+    <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
+      <Link to="/" style={{ margin: '0 10px', color: '#fff' }}>Home</Link>
+      <Link to="/about" style={{ margin: '0 10px', color: '#fff' }}>About</Link>
+      <Link to="/services" style={{ margin: '0 10px', color: '#fff' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '0 10px', color: '#fff' }}>Contact</Link>
     </nav>
   );
 }
 
 export default Navbar;
+
+
