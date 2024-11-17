d74079dfc0138ad37936ae23703e372b91f5a6f5
kalid-2024 <kalidmohammed236@gmail.com>
2024-11-17 15:16:30 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+    <>
+        <footer style={{ textAlign: 'center' , color: 'red' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+        </>
+    )
+}
+
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+    <>
+        <main style={{ color: 'green' , textAlign: 'center' , fontFamily: 'sans-serif' , fontSize: '32px' , fontStyle: 'italic'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    </>
+    )
+}
+
+export default MainContent
