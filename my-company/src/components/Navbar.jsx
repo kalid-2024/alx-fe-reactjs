67a01975847e7a592425d303ea41d13aadc4fcbf
kalid-2024 <kalidmohammed236@gmail.com>
2024-11-17 23:36:13 +0300

    added display:flex and justifyContent style properties to the Navbar.jsx file

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
+    <nav style={{ display:'flex',  justifyContent:'center',padding: '10px', backgroundColor: '#f0f0f0' }}>
       <Link to="/" style={{ margin: '0 15px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 15px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 15px' }}>Services</Link>
