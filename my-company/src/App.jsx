dc4fd8d7629286642f69319d66c204854abdd3bd
kalid-2024 <kalidmohammed236@gmail.com>
2024-11-17 22:17:01 +0300

    Build a Simple Company Website with React

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import About from  './components/About'
 import Services from './components/Services';
