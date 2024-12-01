c0c6adceaeb91c5fea228f84fbca5af14c17e04b
kalid-2024 <kalidmohammed236@gmail.com>
2024-12-01 21:21:57 +0300

    protected route implementation correction in the App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import DynamicRouting from './components/BlogPost'
 import BlogPost from './components/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
       <Routes>
           <Route path="/"  element={<App />} />
           <Route path="/blog/:id"  element={<BlogPost/>} />
+          <Route path="/profile"  element={<ProtectedRoute/>} />
               
       </Routes>
 
