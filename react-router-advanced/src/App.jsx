39d9a23eddea19accb0a548487f3dd50379e775a
kalid-2024 <kalidmohammed236@gmail.com>
2024-12-01 21:05:46 +0300

    correction on app.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import DynamicRouting from './components/userProfiles'
+import DynamicRouting from './components/BlogPost'
+import BlogPost from './components/BlogPost';
 
 function App() {
   
@@ -12,7 +13,7 @@ function App() {
 
       <Routes>
           <Route path="/"  element={<App />} />
-          <DynamicRouting/>
+          <BlogPost/>
       </Routes>
 
     </Router>
--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Route, Switch, Link, useParams } from 'react-router-dom';
 
-// Component that displays user information based on URL parameter
-const User = () => {
+// Component that displays blog information based on URL parameter
+const Blog = () => {
     // useParams hook is used to access route parameters
-    const { userId } = useParams();
-    return <h3>User ID: {userId}</h3>;
+    const { blogId } = useParams();
+    return <h3>Blog ID: {blogId}</h3>;
 };
 
-const DynamicRouting = () => (
+const BlogPost = () => (
     <Router>
         <div>
             <nav>
-                <Link to="/user/1">User 1</Link>
-                <Link to="/user/2">User 2</Link>
+                <Link to="/blog/1">Blog 1</Link>
+                <Link to="/blog/2">Blog 2</Link>
             </nav>
             <Switch>
-                {/* Dynamic route with a userId parameter */}
-                <Route path="/user/:userId" component={User} />
+                {/* Dynamic route with a blogId parameter */}
+                <Route path="/blog/:blogId" component={Blog} />
                 {/* Default route to Home component */}
                 <Route path="/">
                     <h2>Home</h2>
@@ -26,4 +26,4 @@ const DynamicRouting = () => (
     </Router>
 );
 
-export default DynamicRouting;
+export default BlogPost;
