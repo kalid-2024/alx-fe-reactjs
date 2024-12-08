fac87899f4a5ac433c7c02c22acb1696470b94e2
kalid-2024 <kalidmohammed236@gmail.com>
2024-12-08 20:11:15 +0300

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
-  // content: [],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   variants: { extend: {} },
   plugins: [],
-}
-
+};
