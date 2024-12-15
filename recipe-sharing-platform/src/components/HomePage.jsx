a6a612698f601126c841b7ba6f0f3550997b1058
kalid-2024 <kalidmohammed236@gmail.com>
2024-12-15 20:17:08 +0300

    Building the Home Page of the Recipe Sharing Platform

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {useState,useEffect} from 'react'
+
+const HomePage = () => {
+
+    const[recipes,setRecipes] = useState([]);
+
+    useEffect(()=>{
+
+        const fetchData = async ()=>{
+            try{
+                const response = await fetch('/src/data.json');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                  }
+                const data = await response.json();
+                setRecipes(data);
+
+            }catch(error){
+                console.error('Error fetching the recipe data:', error);
+            }
+        };
+        fetchData();
+
+    }, [])
+
+  return (
+    <div>
+        <h1>HomePage</h1>
+        {recipes.length ===0?(<p>Loading Recipes...</p>):(
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {
+                    recipes.map((recipe)=>(
+                        <div key={recipe.id} class="bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm hover:shadow-xl">
+                            <img src={recipe.image} alt={recipe.title}  class="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md:w-36 h-36  hover:scale-110  transition-transform duration-300 ease-in-out"/>
+                            <h2 class="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">{recipe.title}</h2>
+                            <p class="text-gray-600 text-base sm:text-sm md:text-base">{recipe.summary}</p>
+                        </div>
+                    ))
+                }
+            </div>
+        )}
+    </div>
+  );
+}
+
+export default HomePage
