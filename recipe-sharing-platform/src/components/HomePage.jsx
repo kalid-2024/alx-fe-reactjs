1acd776d25edc24693521d466ebd40565462095a
kalid-2024 <kalidmohammed236@gmail.com>
2024-12-15 20:19:58 +0300

    included 'className'in the HomePage component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -28,13 +28,13 @@ const HomePage = () => {
     <div>
         <h1>HomePage</h1>
         {recipes.length ===0?(<p>Loading Recipes...</p>):(
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {
                     recipes.map((recipe)=>(
-                        <div key={recipe.id} class="bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm hover:shadow-xl">
-                            <img src={recipe.image} alt={recipe.title}  class="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md:w-36 h-36  hover:scale-110  transition-transform duration-300 ease-in-out"/>
-                            <h2 class="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">{recipe.title}</h2>
-                            <p class="text-gray-600 text-base sm:text-sm md:text-base">{recipe.summary}</p>
+                        <div key={recipe.id} className="bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm hover:shadow-xl">
+                            <img src={recipe.image} alt={recipe.title}  className="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md:w-36 h-36  hover:scale-110  transition-transform duration-300 ease-in-out"/>
+                            <h2 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">{recipe.title}</h2>
+                            <p className="text-gray-600 text-base sm:text-sm md:text-base">{recipe.summary}</p>
                         </div>
                     ))
                 }
