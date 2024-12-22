4dfff29dd40fb152eafea4f2cd04b7c2d398f160
kalid-2024 <kalidmohammed236@gmail.com>
2024-12-22 23:33:43 +0300

    @import 'tailwindcss/base';

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css'
 import './App.css'
 import Search from './components/Search'
 
