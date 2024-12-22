82a8ab8b853d048fab8be680efdb62248c167e7a
kalid-2024 <kalidmohammed236@gmail.com>
2024-12-22 20:35:01 +0300

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
-import Search from './components/search'
+import Search from './components/Search'
 
 
 function App() {
